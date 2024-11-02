8b6ac45b39e7721b6ee6810ebc2d853307ce13a1
Neo <lanqing.ji@foxmail.com>
2024-11-02 18:10:58 +0800

    Improved the autosar loader

--- cookbook/autosar_loader.py
+++ cookbook/autosar_loader.py
@@ -1,12 +1,9 @@
 from typing import AsyncIterator, Iterator
-
 from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents import Document
-
 from pypdf import PdfReader
 
 class AutosarLoader(BaseLoader):
-    """An example document loader that reads a file line by line."""
 
     def __init__(self, file_path: str) -> None:
         """Initialize the loader with a file path.
@@ -18,11 +15,9 @@ class AutosarLoader(BaseLoader):
 
 
     def lazy_load(self) -> Iterator[Document]:  # <-- Does not take any arguments
-        """A lazy loader that reads a file line by line.
 
-        When you're implementing lazy load methods, you should use a generator
-        to yield documents one by one.
-        """
+        # A lazy loader that reads a file line by line.
+
         str_list = []
         def visitor_body(text, cm, tm, font_dict, font_size):
             nonlocal str_list
@@ -37,21 +32,22 @@ class AutosarLoader(BaseLoader):
             page_number = 0
             for page in reader.pages[0:5]:
                 page.extract_text(visitor_text=visitor_body)
+                file_name = self.file_path.split('/')[-1]
                 yield Document(
                     page_content="".join(str_list),
-                    metadata={"line_number": page_number, "source": self.file_path},
+                    metadata={"page_number": page_number, "source": file_name},
                 )
                 page_number += 1
 
+
 if __name__ == "__main__":
+
     from pathlib import Path
     cookbook_dir = Path(__file__).parent
     pdf_path = cookbook_dir.joinpath("data/AUTOSAR/AUTOSAR_CP_SWS_DiagnosticCommunicationManager-54-78.pdf")
 
-    loader = AutosarLoader(pdf_path)
+    loader = AutosarLoader(str(pdf_path))
 
     ## Test out the lazy load interface
     for doc in loader.lazy_load():
-        print()
-        print(type(doc))
-        #print(doc)
+        print(doc, '\n')
